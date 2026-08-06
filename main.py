2f74132987a269a3c6cdb0ccfe1b1c896af8f3fb
Deniz Bitmez <denizbitmez22@gmail.com>
2026-08-06 18:47:04 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -54,3 +54,31 @@ def create_task(taskData:TaskCreate):
 
     task.append(new_task)
     return new_task
+
+class TaskUpdate(BaseModel):
+    title:str | None=None
+    done:bool | None=None
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id:int, task_data:TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            if task_data.title is not None:
+                if task_data.title.strip() == "":
+                    raise HTTPException(status_code=400, detail={"error": "Title cannot be empty"})
+                task["title"] = task_data.title
+            if task_data.done is not None:
+                task["done"] = task_data.done
+
+            return task
+
+    raise HTTPException(status_code=404, detail={"error": f"Task {task_id} not found"})
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(index)
+            return 
+
+    raise HTTPException(status_code=404, detail={"error": f"Task {task_id} not found"})
